ec15ae14b0df5c5d937d00277c5655c204e392fb
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-09-03 09:43:50 +0600

    footer added

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
 const loadCategory = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayCategory(data.data.news_category))
-    .catch(error => console.log(error))
+        .then(res => res.json())
+        .then(data => displayCategory(data.data.news_category))
+        .catch(error => console.log(error))
 }
 
-const displayCategory = categories =>{
+const displayCategory = categories => {
     console.log(categories);
-    
-     const categoryContainer = document.getElementById('category-container');
-     categories.forEach(category => {
-     const categoryList = document.createElement('div'); 
-    categoryList.innerHTML = `
+
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(category => {
+        const categoryList = document.createElement('div');
+        categoryList.innerHTML = `
      <button type="button" class="btn btn-outline-primary list-group-item m-2 rounded" onclick = "loadNewsDetails('${category.category_id}')">${category.category_name}</button> 
     `
-    categoryContainer.appendChild(categoryList);
-     }) 
-     
+        categoryContainer.appendChild(categoryList);
+    })
+
 }
 
-const loadNewsDetails = async(newsId) => {
+const loadNewsDetails = async (newsId) => {
     // console.log(newsId);
     const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`
 
-    try{
+    try {
         const res = await fetch(url);
         const data = await res.json();
         displayNewsDetails(data.data);
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
- 
+
 }
 
 const displayNewsDetails = details => {
     console.log(details);
 
     //sorting all the fetching data by total_view
-    details.sort((a , b) =>
-    {
+    details.sort((a, b) => {
         return b.total_view - a.total_view;
     })
- 
+
     //items found massage
     const noPhoneMeassage = document.getElementById('message');
-    if(details.length !== 0){
+    if (details.length !== 0) {
         noPhoneMeassage.innerHTML = `<h2 class = "ms-3">${details.length} items found for this category</h2>`
-    }else{
+    } else {
         noPhoneMeassage.innerHTML = `<h2>No items found for this category</h2>`
     }
-    
+
     const displayNewsContainer = document.getElementById('display-news-container');
     displayNewsContainer.innerHTML = ``;
-    
-    details.forEach(detail =>{
-    const displayDiv = document.createElement('div');
-    displayDiv.classList.add('col');
-    displayDiv.innerHTML = `
+
+    details.forEach(detail => {
+        const displayDiv = document.createElement('div');
+        displayDiv.classList.add('col');
+        displayDiv.innerHTML = `
     <div class="card h-100 p-3">
     <img src="${detail.thumbnail_url}" class="card-img-top" alt="...">
     <div class="card-body">
@@ -67,7 +66,7 @@ const displayNewsDetails = details => {
         <img style="width: 50px; height: 50px;" class = " border-radious" src ="${detail.author.img}">
         <span">${detail.author.name ? detail.author.name : 'No name Found'}</span> 
 
-        <p class = "ms-2 mt-2 d-inline"><i class="fa-solid fa-eye"></i> ${detail.total_view ? detail.total_view : '0' }M </p>
+        <p class = "ms-2 mt-2 d-inline"><i class="fa-solid fa-eye"></i> ${detail.total_view ? detail.total_view : '0'}M </p>
         <p class = "ms-5 ps-5">
             <i class="fa-solid fa-star-half-stroke"></i>
             <i class="fa-regular fa-star"></i>
@@ -81,18 +80,18 @@ const displayNewsDetails = details => {
       </div>
     </div>
     `
-    displayNewsContainer.appendChild(displayDiv);
-    
+        displayNewsContainer.appendChild(displayDiv);
+
     })
-    
+
 }
 
 // spinner function
 const toggleSpinner = isLoading => {
     const loadingSpinner = document.getElementById('loader');
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('d-none');
-    }else{
+    } else {
         loadingSpinner.classList.add('d-none');
     }
 }
@@ -102,7 +101,7 @@ const toggleSpinner = isLoading => {
 const displayModal = (img, title, author, authorImg, view, pDate) => {
     console.log(img, title, author, authorImg, view, pDate);
     const modalContainer = document.getElementById('exampleModalLabel');
-    modalContainer.innerText = title; 
+    modalContainer.innerText = title;
     const modalDetail = document.getElementById('modalDetail');
     modalDetail.innerHTML = ` 
     <img " class = "img-fluid" src="${img}" alt="">
@@ -122,5 +121,5 @@ const displayModal = (img, title, author, authorImg, view, pDate) => {
     
     `
 }
-
+loadNewsDetails('08');
 loadCategory();
