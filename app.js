0a3386670ac7be330b1bacf5a002ed94461878d7
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-09-03 08:51:31 +0600

    display all news

--- app.js
+++ app.js
@@ -20,4 +20,57 @@ const displayCategory = categories =>{
      
 }
 
+const loadNewsDetails = async(newsId) => {
+    // console.log(newsId);
+    const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`
+
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsDetails(data.data);
+    }catch(error){
+        console.log(error);
+    }
+ 
+}
+
+const displayNewsDetails = details => {
+    console.log(details);
+
+    const displayNewsContainer = document.getElementById('display-news-container');
+    displayNewsContainer.innerHTML = ``;
+    
+    details.forEach(detail =>{
+    const displayDiv = document.createElement('div');
+    displayDiv.classList.add('col');
+    displayDiv.innerHTML = `
+    <div class="card h-100 p-3">
+    <img src="${detail.thumbnail_url}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title fw-bolder">${detail.title}</h5>
+      <p class="card-text">${detail.details.slice(0, 200)}...</p>
+      <div>
+        <img style="width: 50px; height: 50px;" class = " border-radious" src ="${detail.author.img}">
+        <span">${detail.author.name ? detail.author.name : 'No name Found'}</span> 
+
+        <p class = "ms-2 mt-2 d-inline"><i class="fa-solid fa-eye"></i> ${detail.total_view ? detail.total_view : '0' }M </p>
+        <p class = "ms-5 ps-5">
+            <i class="fa-solid fa-star-half-stroke"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+        </p>
+
+        <button onclick = "displayModal('${detail.image_url}', '${detail.title}', '${detail.author.name ? detail.author.name : 'No name Found'}', '${detail.author.img}', '${detail.total_view ? detail.total_view : '0'}M', '${detail.author.published_date}')" class = "border-0 bg-white ms-auto d-flex justify-content-end" data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="fa-sharp fa-solid fa-arrow-right text-primary fw-bolder fs-3"></i> </button>
+        
+      </div>
+    </div>
+    `
+    displayNewsContainer.appendChild(displayDiv);
+    
+    })
+    
+}
+
 loadCategory();
